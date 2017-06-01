548f9c7f074907d4ef389923328252eaeb9ead63
junnie <junnie@iflytek.com>
2017-06-01 19:53:01 +0800

    init
    init

---
+++ audio/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,11 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_a78_audio_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
